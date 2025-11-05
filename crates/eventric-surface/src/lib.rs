0e24752a63986ed4f016e6b6cf01d57e216be011
Andrew Cherry <andrew@xyncro.com>
2025-11-05 17:23:23 +0000

    temp test

--- crates/eventric-surface/src/lib.rs
+++ crates/eventric-surface/src/lib.rs
@@ -1,1 +1,14 @@
+#[allow(dead_code)]
+fn testable() -> bool {
+    true
+}
 
+#[cfg(test)]
+mod tests {
+    use crate::testable;
+
+    #[test]
+    fn temp() {
+        assert!(testable());
+    }
+}
